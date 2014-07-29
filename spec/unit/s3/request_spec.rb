3ba04e9b66dfb237ae717562e30df0f81371950c
Pete Yandell <pete@notahat.com>
2014-07-29 22:51:48 +1000

    Fill out missing Request specs.

--- spec/unit/s3/request_spec.rb
+++ spec/unit/s3/request_spec.rb
@@ -4,19 +4,38 @@ describe FakeAWS::S3::Request do
   subject { described_class.new(env) }
 
   context "#method" do
-    it "works"
+    it "returns the request method" do
+      request = described_class.new("REQUEST_METHOD" => "GET")
+      expect(request.method).to eq("GET")
+    end
   end
 
   context "#content_type" do
-    it "works"
+    it "returns the content type" do
+      request = described_class.new("CONTENT_TYPE" => "text/plain")
+      expect(request.content_type).to eq("text/plain")
+    end
   end
 
   context "#content" do
-    it "works"
+    it "reads and returns the Rack input" do
+      rack_input = double("rack.input")
+      expect(rack_input).to receive(:read) { "foo" }
+      request = described_class.new("rack.input" => rack_input)
+      expect(request.content).to eq("foo")
+    end
   end
 
   context "#http_headers" do
-    it "works"
+    it "returns the HTTP headers" do
+      request = described_class.new("HTTP_X_FOO" => "foo", "HTTP_X_BAR" => "bar")
+      expect(request.http_headers).to eq("x-foo" => "foo", "x-bar" => "bar")
+    end
+
+    it "ignores non-HTTP headers" do
+      request = described_class.new("FOO" => "foo")
+      expect(request.http_headers).to eq({})
+    end
   end
 
   context "with a path-style request" do
