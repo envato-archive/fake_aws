1db3281eab3a1c76562934e089a57d2fe99817af
Pete Yandell <pete@notahat.com>
2014-07-29 23:27:51 +1000

    Improve the request style integration specs.

--- spec/integration/s3/request_styles_spec.rb
+++ spec/integration/s3/request_styles_spec.rb
@@ -12,11 +12,7 @@ describe "Request styles:" do
     File.write(File.join(s3_path, bucket, file_name), file_contents)
   end
 
-  context "virtual hosted-style GET Object" do
-    def get_example_file(key)
-      connection.get("http://#{bucket}.s3.amazonaws.com/#{key}")
-    end
-
+  shared_examples "GET Object" do
     it "returns a 200" do
       response = get_example_file(file_name)
       expect(response.status).to eq(200)
@@ -28,6 +24,22 @@ describe "Request styles:" do
     end
   end
 
+  context "path-style GET Object" do
+    def get_example_file(key)
+      connection.get("http://s3.amazonaws.com/#{bucket}/#{key}")
+    end
+
+    include_examples "GET Object"
+  end
+
+  context "virtual hosted-style GET Object" do
+    def get_example_file(key)
+      connection.get("http://#{bucket}.s3.amazonaws.com/#{key}")
+    end
+
+    include_examples "GET Object"
+  end
+
   context "CNAME-style GET Object" do
     let(:bucket) { "mah-bucket.mah-domain.com" }
 
@@ -35,15 +47,7 @@ describe "Request styles:" do
       connection.get("http://#{bucket}/#{key}")
     end
 
-    it "returns a 200" do
-      response = get_example_file(file_name)
-      expect(response.status).to eq(200)
-    end
-
-    it "returns the contents of the file" do
-      response = get_example_file(file_name)
-      expect(response.body).to eq(file_contents)
-    end
+    include_examples "GET Object"
   end
 
 end
